cfbc0a0059371b90e6ec6614a595c33f693f73f1
Leandro Gonçalves <159172886+lleandrogon@users.noreply.github.com>
2024-04-08 16:58:28 -0300

    site divas

--- js/index.js
+++ js/index.js
@@ -1,1 +1,7 @@
-document.getElementById
+var idade, eleitor, resultado;
+idade = 71;
+eleitor = (idade < 18) ? "Não, Eleitor" : "Sim, eleitor"
+
+resultado = (idade === 65 || idade === 72);
+
+alert(resultado)
